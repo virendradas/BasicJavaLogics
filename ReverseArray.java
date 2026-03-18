2958657b9b9fe03a864c82a185bbe9269f0de2a1
virendradas <virendradas2507@gmail.com>
2026-03-18 12:42:51 +0530

    Besic java logics qustions

---
+++ ReverseArray.java
@@ -0,0 +1,30 @@
+class ReverseArray
+{
+    static int[] revArray(int[] arr)
+    {
+        int start = 0;
+        int end = arr.length-1;
+
+        while(start<end)
+        {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+
+            start++;
+            end--;
+        }
+
+        return arr;
+    
+    }
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5};
+        int[] rev = revArray(arr);
+
+        for(int i=0; i<rev.length; i++)
+        {
+            System.out.print(rev[i] + " ");
+        }
+    }
+}
